a9b27052b14d1b8c8ccebcdcaf3b33c8f3caed8d
Takumi Ooshiro <e165703@ie.u-ryukyu.ac.jp>
2017-02-05 14:38:16 +0900

    a

--- report7/jp/ac/uryukyu/ie/e165703/Numeron.java
+++ report7/jp/ac/uryukyu/ie/e165703/Numeron.java
@@ -1,8 +1,56 @@
 package jp.ac.uryukyu.ie.e165703;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 /**
  * Created by e165703 on 2017/02/03.
  */
-public class Numeron {
-    int[] inas = new int[3];
+public class Numeron{
+    int[] player_answer = new int[3];
+    int[] answer;
+    int eat,bite;
+    boolean player_win = false;
+    public Numeron(int[] answer){
+        this.answer = answer;
+    }
+
+    public boolean isPlayer_win() {
+        return player_win;
+    }
+
+    public void settei(){
+        BufferedReader br
+                = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < 3; i++) {
+            System.out.print((i + 1) + "個目 : ");
+            try {
+                player_answer[i] = Integer.parseInt(br.readLine());
+            } catch (NumberFormatException e) {
+                System.err.println("数値を入力してください");
+                i--;
+            } catch (IOException e) {
+                System.err.println("もう一度入力してください");
+                i--;
+            }
+        }
+    }
+    public void check_the_answer(){
+        eat = 0;
+        bite = 0;
+        for (int i = 0; i < answer.length; i++) {
+            for (int j = 0; j < answer.length; j++) {
+                if (i == j && player_answer[i] == answer[j]) {
+                    eat++;
+                } else if (player_answer[i] == answer[j]) {
+                    bite++;
+                }
+            }
+        }
+        //終了判断　ヒットが3つになったら終了
+        System.out.println(eat + "EAT" + " " + bite + "BITE");
+        if(eat == 3){
+            player_win = true;
+        }
+    }
+
 }
